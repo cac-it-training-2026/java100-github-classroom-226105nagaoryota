a5b01441919c08e494be7f572c006788577a8d1c
rnagao <rnagao@C010423416.zipangu.cac.co.jp>
2026-04-14 16:42:43 +0900

    lesson1,challenge10にて指定されていたため、クラスをこの場所に作成しました。

---
+++ java100_questions_cac/src/main/java/jp/co/sss/java100_questions_cac/lesson04/challenge06/Advertisement.java
@@ -0,0 +1,19 @@
+package jp.co.sss.java100_questions_cac.lesson04.challenge06;
+
+public class Advertisement {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		System.out.println("マカロン屋さんで働いてみませんか！");
+
+		System.out.println("\n勤務時間");
+		System.out.println("9:00~19:00(水曜定休)");
+
+		System.out.println("\n時給1300円");
+
+		System.out.println("\n週1,3時間からでもOK!");
+		System.out.println("皆さんのご応募待ってます！");
+	}
+
+}
